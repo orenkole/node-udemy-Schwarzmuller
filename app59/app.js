4c9d7c2a69fbff3848faec06da4f1ba15cdf23b4
Oleh <orenkole@gmail.com>
2020-08-08 18:12:29 +0300

    68 filtering pathes

--- app59/app.js
+++ app59/app.js
@@ -2,15 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended: false}));
-app.use('/add-product', (req, res, next) => {
-  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>')
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).send('<h1>Page not found</h1>')
 })
-app.post('/product', (req, res, next) => {
-  console.log(req.body)
-  res.redirect('/');
-})
-app.use('/', (req, res, next) => {
-  res.send('<h1>Hello from express</h1>')
-})
+
 app.listen(3000);

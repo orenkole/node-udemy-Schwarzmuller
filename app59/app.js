d71b9d7b0f3d54dfbfe1d86c1d5269252534b345
Oleh <orenkole@gmail.com>
2020-08-08 14:21:06 +0300

    added body-parser' to parse requests

--- app59/app.js
+++ app59/app.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: false}));
 app.use('/add-product', (req, res, next) => {
-  console.log('in the next middleware');
-  res.send('<h1>Add product</h1>')
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>')
+})
+app.post('/product', (req, res, next) => {
+  console.log(req.body)
+  res.redirect('/');
 })
 app.use('/', (req, res, next) => {
-  console.log('in the next middleware');
   res.send('<h1>Hello from express</h1>')
 })
 app.listen(3000);

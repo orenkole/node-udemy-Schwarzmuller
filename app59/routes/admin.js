b4fc285ddf0541e042963c6858f1b99eda6e168e
Oleh <orenkole@gmail.com>
2020-08-08 18:46:51 +0300

    74. Using path.js, path.dirname(process.mainModule.filename)

--- app59/routes/admin.js
+++ app59/routes/admin.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 
+const rootDir = require('../util/path')
+
 router.get('/add-product', (req, res, next) => {
   res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))
 })

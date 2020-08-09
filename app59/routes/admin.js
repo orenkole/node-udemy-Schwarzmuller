3c46fc7d4901291f07464e2da1e573f80ac82050
Oleh <orenkole@gmail.com>
2020-08-09 22:07:45 +0300

    added variables to handle POST request from form

--- app59/routes/admin.js
+++ app59/routes/admin.js
@@ -4,12 +4,15 @@ const router = express.Router();
 
 const rootDir = require('../util/path')
 
+const products = [];
+
 router.get('/add-product', (req, res, next) => {
   res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))
 })
 router.post('/add-product', (req, res, next) => {
-  console.log(req.body)
+  products.push({title: req.body.title})
   res.redirect('/');
 })
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
--- app59/routes/shop.js
+++ app59/routes/shop.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const path = require('path')
+
+const adminData = require('./admin');
+
 const router = express.Router();
 router.get('/', (req, res, next) => {
+  console.log(adminData.products);
   res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'))
 })
 module.exports = router;

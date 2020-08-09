e6ac79c9cbc5c7d838d654a40bf3c831fa2f0c41
Oleh <orenkole@gmail.com>
2020-08-09 23:26:38 +0300

    began to use pug

--- app59/app.js
+++ app59/app.js
@@ -3,6 +3,9 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
@@ -12,7 +15,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+  res.status(404).render('404');
 })
 
 app.listen(3000);
--- app59/routes/admin.js
+++ app59/routes/admin.js
@@ -7,7 +7,7 @@ const rootDir = require('../util/path')
 const products = [];
 
 router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))
+  res.render('add-product', {pageTitle: "Add product", path: "/admin/add-product"})
 })
 router.post('/add-product', (req, res, next) => {
   products.push({title: req.body.title})
